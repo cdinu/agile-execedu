4f205cedd48f8a9fa6c697c40ff664d87df0ffa8
Cristian Dinu <cristian.dinu@readfwd.com>
2014-07-23 15:20:50 +0300

    Interim work

---
+++ js/main.js
@@ -0,0 +1,59 @@
+define([
+  '../addons/lfa-backstage/js/main',
+  '../addons/lfa-exercises/js/main',
+  './rainbow'
+], function (Backstage, Exercises) {
+  window.Exercises = Exercises;
+  window.firstTimeDoneMessages = [
+    'Bravo!',
+    'Felicitări!',
+    'Excelent!',
+    'Corect!',
+    'Super!',
+    'Impecabil!',
+    'Grozav!'
+  ];
+
+  var greetings = ['Bun venit!', 'Salut!', 'Salutări!', 'Bună!', 'Ce faci?'];
+
+  var hh = (new Date()).getHours();
+
+  if (hh >= 2 && hh < 12) {
+    greetings.push('Bună dimineața!');
+  }
+  else if (hh > 5 || hh < 2) {
+    greetings.push('Bună seara!');
+  } else {
+    greetings.push('Bună ziua!');
+  }
+
+  var useCounter = window.IEProofLocalStorage.getItem('useCounter') || 0;
+  useCounter = parseInt(useCounter) + 1;
+
+  var message = greetings[Math.min(Math.floor(Math.random() *greetings.length*1.5), greetings.length-1)];
+
+
+  if(useCounter < 2){
+     message = 'Apasă pentru a afla cum se folosește acest manual!';
+  }
+
+  window.setTimeout(function () {
+    window.App.trigger('avatar:mood', {
+      mood: 'smile',
+      message: message,
+      interval: 5000,
+      icon: 'fa-smile-o'
+    });
+  }, 4000);
+
+  window.IEProofLocalStorage.setItem('useCounter', useCounter);
+
+  if(!IEProofLocalStorage.teacherMode) {
+    IEProofLocalStorage.setItem('teacherMode', 'off');
+  }
+
+  window.Exercises = Exercises;
+  window.App.book.on('render', function() {
+    $('#textbook a').attr('target', '_blank');
+  });
+});
